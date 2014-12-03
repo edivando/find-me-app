74db1bd861b37adb7ba1db446f67ce9812024a2a
Yuri Anfrisio Reis <Yuri@Yuris-Mac.local>
2014-12-03 18:21:56 -0300

    Criando classes de permission

---
+++ FindMeApp/FindMeApp/PermissionInfo.m
@@ -0,0 +1,13 @@
+//
+//  PermissionInfo.m
+//  FindMeApp
+//
+//  Created by Yuri Anfrisio Reis on 12/3/14.
+//  Copyright (c) 2014 BEPiD. All rights reserved.
+//
+
+#import "PermissionInfo.h"
+
+@implementation PermissionInfo
+
+@end
